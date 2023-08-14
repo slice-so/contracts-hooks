ba8cff5089f3751e889f711a40c08f681f7acca9
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-08-14 14:32:00 +0200

    feat: add slc

---
+++ contracts/utils/SLC.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import {ERC20} from "solmate/src/tokens/ERC20.sol";
+
+contract SLC is ERC20 {
+    error Unauthorized();
+
+    address public immutable productsModule;
+    address public immutable fundsModule;
+
+    constructor(address productsModule_, address fundsModule_) ERC20("Slice", "SLC", 18) {
+        productsModule = productsModule_;
+        fundsModule = fundsModule_;
+    }
+
+    function mint(address to, uint256 amount) external {
+        if (msg.sender != productsModule && msg.sender != fundsModule) {
+            revert Unauthorized();
+        }
+
+        _mint(to, amount);
+    }
+}
