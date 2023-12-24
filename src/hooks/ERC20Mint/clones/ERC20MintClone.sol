663c998d62eecd99a29159a9a7829bba8084f9b4
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-12-24 01:59:45 +0100

    add: ERC20Mint

---
+++ src/hooks/ERC20Mint/clones/ERC20MintClone.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../../../extensions/Purchasable/SlicerPurchasableClone.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+/**
+ * ERC20Mint purchase hook.
+ *
+ * Mints 1000 tokens for each unit purchased.
+ */
+contract ERC20MintClone is Initializable, ERC20Upgradeable, SlicerPurchasableClone {
+    // =============================================================
+    //                           Errors
+    // =============================================================
+
+    error WrongProductId();
+
+    // =============================================================
+    //                           Storage
+    // =============================================================
+
+    uint256 public allowedProductId;
+
+    // =============================================================
+    //                         Constructor
+    // =============================================================
+
+    /**
+     * @notice Initializes the contract.
+     *
+     * @param productsModuleAddress_ {ProductsModule} address
+     * @param slicerId_ ID of the slicer linked to this contract
+     * @param name_ Name of the ERC721 contract
+     * @param symbol_ Symbol of the ERC721 contract
+     * @param allowedProductId_ ID of the product allowed to be purchased
+     */
+    function initialize(
+        address productsModuleAddress_,
+        uint256 slicerId_,
+        string memory name_,
+        string memory symbol_,
+        uint256 allowedProductId_
+    ) external initializer {
+        __SlicerPurchasableClone_init(productsModuleAddress_, slicerId_);
+        __ERC20_init(name_, symbol_);
+
+        allowedProductId = allowedProductId_;
+    }
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    // =============================================================
+    //                         Purchase hook
+    // =============================================================
+
+    /**
+     * @notice Overridable function to handle external calls on product purchases from slicers. See {ISlicerPurchasable}
+     */
+    function onProductPurchase(
+        uint256 slicerId,
+        uint256 productId,
+        address buyer,
+        uint256 quantity,
+        bytes memory,
+        bytes memory
+    ) public payable override onlyOnPurchaseFrom(slicerId) {
+        if (allowedProductId != 0) {
+            if (productId != allowedProductId) revert WrongProductId();
+        }
+
+        // Mint tokens
+        _mint(buyer, quantity * 1e21);
+    }
+}
---
+++ src/hooks/ERC20Mint/immutable/ERC20MintImmutable.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../../../extensions/Purchasable/SlicerPurchasableConstructor.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * ERC20Mint purchase hook.
+ *
+ * Mints 1000 tokens for each unit purchased.
+ */
+contract ERC20MintImmutable is ERC20, SlicerPurchasableConstructor {
+    // =============================================================
+    //                           Errors
+    // =============================================================
+
+    error WrongProductId();
+
+    // =============================================================
+    //                           Storage
+    // =============================================================
+
+    uint256 public immutable allowedProductId;
+
+    // =============================================================
+    //                         Constructor
+    // =============================================================
+
+    /**
+     * @notice Initializes the contract.
+     *
+     * @param productsModuleAddress_ {ProductsModule} address
+     * @param slicerId_ ID of the slicer linked to this contract
+     * @param name_ Name of the ERC721 contract
+     * @param symbol_ Symbol of the ERC721 contract
+     * @param allowedProductId_ ID of the product allowed to be purchased
+     */
+    constructor(
+        address productsModuleAddress_,
+        uint256 slicerId_,
+        string memory name_,
+        string memory symbol_,
+        uint256 allowedProductId_
+    ) SlicerPurchasableConstructor(productsModuleAddress_, slicerId_) ERC20(name_, symbol_) {
+        allowedProductId = allowedProductId_;
+    }
+
+    // =============================================================
+    //                         Purchase hook
+    // =============================================================
+
+    /**
+     * @notice Overridable function to handle external calls on product purchases from slicers. See {ISlicerPurchasable}
+     */
+    function onProductPurchase(
+        uint256 slicerId,
+        uint256 productId,
+        address buyer,
+        uint256 quantity,
+        bytes memory,
+        bytes memory
+    ) public payable override onlyOnPurchaseFrom(slicerId) {
+        if (allowedProductId != 0) {
+            if (productId != allowedProductId) revert WrongProductId();
+        }
+
+        // Mint tokens
+        _mint(buyer, quantity * 1e21);
+    }
+}
